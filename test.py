e50e5084e865d2c4a9f477461128c4067d413fa0
Bin Yu <andrew.yu@hotmail.com>
2018-10-06 00:46:44 -0400

    test.py without opening browser

--- test.py
+++ test.py
@@ -8,16 +8,22 @@ Created on Fri Oct  5 23:55:23 2018
 
 from selenium import webdriver
 import time
+#from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')     # 打开 Chrome 浏览器
+chrome_options = webdriver.chrome.options.Options()
+chrome_options.add_argument("--headless")       # define headless
+
+
+#driver = webdriver.Chrome('/usr/local/bin/chromedriver')     # open Chrome browser
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options) # without opening browser
 
 driver.get("https://morvanzhou.github.io/")
 driver.find_element_by_xpath(u"//img[@alt='强化学习 (Reinforcement Learning)']").click()
 driver.get_screenshot_as_file("./screenshot1.png")
-time.sleep(2)
+#time.sleep(2)
 driver.find_element_by_link_text("About").click()
 driver.get_screenshot_as_file("./screenshot2.png")
-time.sleep(2)
+#time.sleep(2)
 driver.find_element_by_link_text(u"赞助").click()
 driver.find_element_by_link_text(u"教程 ▾").click()
 driver.find_element_by_link_text(u"数据处理 ▾").click()
